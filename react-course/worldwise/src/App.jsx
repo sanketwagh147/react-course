be631940b881d7b10e669b42131dc68c658ab219
Sanket Wagh <sanketwagh147@gmail.com>
2023-12-31 15:39:25 +0530

    implement fake login logut ch:2239

--- react-course/worldwise/src/App.jsx
+++ react-course/worldwise/src/App.jsx
@@ -12,35 +12,38 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
 	return (
 		// Using Routes in react app
-		<CitiesProvider>
-			<BrowserRouter>
-				<Routes>
-					{/* This both lines do same */}
-					{/* <Route path="/" element={<Homepage />} /> */}
-					<Route index element={<Homepage />} />
+		<AuthProvider>
+			<CitiesProvider>
+				<BrowserRouter>
+					<Routes>
+						{/* This both lines do same */}
+						{/* <Route path="/" element={<Homepage />} /> */}
+						<Route index element={<Homepage />} />
 
-					<Route path="/product" element={<Product />} />
-					<Route path="/pricing" element={<Pricing />} />
-					<Route path="/login" element={<Login />} />
-					{/* Nested Routes */}
-					<Route path="/app" element={<AppLayout />}>
-						{/* defaul index route if nothing matches */}
-						{/* Use <Navigate> to redirect */}
-						{/* replace is used so that back button works (for history*/}
-						<Route index element={<Navigate replace to="cities" />} />
-						<Route path="cities" element={<CityList />} />
-						<Route path="cities/:id" element={<City />} />
-						<Route path="countries" element={<CountryList />} />
-						<Route path="form" element={<Form />} />
-					</Route>
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</CitiesProvider>
+						<Route path="/product" element={<Product />} />
+						<Route path="/pricing" element={<Pricing />} />
+						<Route path="/login" element={<Login />} />
+						{/* Nested Routes */}
+						<Route path="/app" element={<AppLayout />}>
+							{/* defaul index route if nothing matches */}
+							{/* Use <Navigate> to redirect */}
+							{/* replace is used so that back button works (for history*/}
+							<Route index element={<Navigate replace to="cities" />} />
+							<Route path="cities" element={<CityList />} />
+							<Route path="cities/:id" element={<City />} />
+							<Route path="countries" element={<CountryList />} />
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
 	);
 }
 
--- react-course/worldwise/src/pages/Login.jsx
+++ react-course/worldwise/src/pages/Login.jsx
@@ -1,16 +1,42 @@
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
 	// PRE-FILL FOR DEV PURPOSES
-	const [email, setEmail] = useState("jack@example.com");
+	const [email, setEmail] = useState("sanket@example.com");
 	const [password, setPassword] = useState("qwerty");
+
+	const { login, isAuthenticated } = useAuth();
+	const navigate = useNavigate();
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		if (email && password) {
+			login(email, password);
+		}
+	}
+
+	// Repace true forget login page history
+	// landing page -> login >app
+	// from app back goes to landing page and not login
+	useEffect(
+		function () {
+			if (isAuthenticated) {
+				navigate("/app", { replace: true });
+			}
+		},
+		[isAuthenticated, navigate]
+	);
 
 	return (
 		<main className={styles.login}>
 			<PageNav />
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<div className={styles.row}>
 					<label htmlFor="email">Email address</label>
 					<input
@@ -32,7 +58,8 @@ export default function Login() {
 				</div>
 
 				<div>
-					<button>Login</button>
+					<Button type="primary">Login</Button>
+					{/* <button>Login</button> */}
 				</div>
 			</form>
 		</main>

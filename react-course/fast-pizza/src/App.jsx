8e65b94a9a1b623882dd0792e8032a6a7740d60b
Sanket Wagh <sanketwagh147@gmail.com>
2024-01-07 23:35:30 +0530

    react router data loading

--- react-course/fast-pizza/src/App.jsx
+++ react-course/fast-pizza/src/App.jsx
@@ -1,3 +1,49 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {
+	action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+// ! Define all routes
+const router = createBrowserRouter([
+	{
+		// ! layout route or parent route
+		// ! No path as it is parent route
+		element: <AppLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/menu",
+				element: <Menu />,
+				// new react router loader
+				loader: menuLoader,
+				errorElement: <Error />,
+			},
+			{ path: "/cart", element: <Cart /> },
+			{
+				path: "/order/new",
+				element: <CreateOrder />,
+				// ! Whenever there will be new form submission the action will be called with the request as parameter
+				action: createOrderAction,
+			},
+			{
+				path: "/order/:orderId",
+				element: <Order />,
+				loader: orderLoader,
+				errorElement: <Error />,
+			},
+		],
+	},
+]);
 export default function App() {
-	return <div>Hello Vite</div>;
+	return <RouterProvider router={router} />;
 }
---
+++ react-course/fast-pizza/src/features/order/CreateOrder.jsx
@@ -0,0 +1,113 @@
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+	/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+		str
+	);
+
+const fakeCart = [
+	{
+		pizzaId: 12,
+		name: "Mediterranean",
+		quantity: 2,
+		unitPrice: 16,
+		totalPrice: 32,
+	},
+	{
+		pizzaId: 6,
+		name: "Vegetale",
+		quantity: 1,
+		unitPrice: 13,
+		totalPrice: 13,
+	},
+	{
+		pizzaId: 11,
+		name: "Spinach and Mushroom",
+		quantity: 1,
+		unitPrice: 15,
+		totalPrice: 15,
+	},
+];
+
+function CreateOrder() {
+	// hook for loader action
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === "submitting";
+	// const [withPriority, setWithPriority] = useState(false);
+	const cart = fakeCart;
+	// ! returns any errors ond data from action hook ( data validation)
+	const formErrors = useActionData();
+
+	return (
+		<div>
+			<h2>Ready to order? Lets go!</h2>
+
+			{/* // ! By default react router will match the closes route so need to write action */}
+			{/* <Form method="POST" action="/order/new"> */}
+			<Form method="POST" action="">
+				<div>
+					<label>First Name</label>
+					<input type="text" name="customer" required />
+				</div>
+
+				<div>
+					<label>Phone number</label>
+					<div>
+						<input type="tel" name="phone" required />
+					</div>
+					{formErrors?.phone && <p>{formErrors.phone}</p>}
+				</div>
+
+				<div>
+					<label>Address</label>
+					<div>
+						<input type="text" name="address" required />
+					</div>
+				</div>
+
+				<div>
+					<input
+						type="checkbox"
+						name="priority"
+						id="priority"
+						// value={withPriority}
+						// onChange={(e) => setWithPriority(e.target.checked)}
+					/>
+					<label htmlFor="priority">Want to yo give your order priority?</label>
+				</div>
+
+				<div>
+					<input type="hidden" name="cart" value={JSON.stringify(cart)} />
+					<button disabled={isSubmitting}>
+						{" "}
+						{isSubmitting ? "Placing order.." : "Order now"}
+					</button>
+				</div>
+			</Form>
+		</div>
+	);
+}
+
+// react router connects this on submit of form
+export async function action({ request }) {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+	const order = {
+		...data,
+		cart: JSON.parse(data.cart),
+		priority: data.priority === "on",
+	};
+	console.log(order);
+
+	// ! data validation step
+	const error = {};
+	if (!isValidPhone(order.phone))
+		error.phone = `Please Enter a correct phone number`;
+
+	const newOrder = await createOrder(order);
+	if (Object.keys(error).length > 0) return error;
+	redirect(`/order/${newOrder.id}`);
+}
+export default CreateOrder;

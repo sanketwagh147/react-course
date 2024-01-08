e95b872b83fdab7ee978a39bf7b78be4dac997e5
Sanket Wagh <sanketwagh147@gmail.com>
2024-01-08 09:32:45 +0530

    setting up tailwind

--- react-course/fast-pizza/src/ui/Header.jsx
+++ react-course/fast-pizza/src/ui/Header.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
-	return (
-		<header>
-			<Link to="/">Fast Pizza Co. </Link>
-			<SearchOrder />
-			<p>Sanket</p>
-		</header>
-	);
+  return (
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast Pizza Co.{" "}
+      </Link>
+      <SearchOrder />
+      <p>Sanket</p>
+    </header>
+  );
 }

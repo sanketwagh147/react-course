f89067617019551b9757cca4f84ccb6857b48a4d
Sanket Wagh <sanketwagh147@gmail.com>
2024-01-13 14:47:01 +0530

    some tailwind propertis

--- react-course/fast-pizza/src/ui/AppLayout.jsx
+++ react-course/fast-pizza/src/ui/AppLayout.jsx
@@ -4,17 +4,19 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 export default function AppLayout() {
-	const navigation = useNavigation();
-	// Global loading state
-	const isLoading = navigation.state === "loading";
-	return (
-		<div className="layout">
-			{isLoading && <Loader />}
-			<Header />
-			<main>
-				<Outlet />
-			</main>
-			<CartOverview />
-		</div>
-	);
+  const navigation = useNavigation();
+  // Global loading state
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+      <div className="overflow-scroll ">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
 }

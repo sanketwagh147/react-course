d4094db36331135018876f9b08cc81722dbc86f4
Sanket Wagh <sanketwagh147@gmail.com>
2024-01-21 12:53:46 +0530

    completed supabase crash vourse

---
+++ react-course/the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+	const { data, error } = await supabase.from("cabins").select("*");
+
+	if (error) {
+		console.error(error);
+		throw new Error("Failed to load cabins from table");
+	}
+	return data;
+}
---
+++ react-course/the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://uuknaakoauvqxlstdbdf.supabase.co";
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1a25hYWtvYXV2cXhsc3RkYmRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4MTkxOTIsImV4cCI6MjAyMTM5NTE5Mn0.AiVVzivjuZfbyVW8wgamD9Hkc5zrzt7P7O_q3zcwgKg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
